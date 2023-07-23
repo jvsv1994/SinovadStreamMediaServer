3cf51f4a2f984c406e90025e3f3f0b02932c76ca
Jorge Victor Solis Vega <jvsv1994@gmail.com>
2023-07-23 14:22:39 -0500

    JS - Enhancements

--- SinovadMediaServer/SinovadMediaServer/Program.cs
+++ SinovadMediaServer/SinovadMediaServer/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using Quartz;
 using SinovadMediaServer.Configuration;
 using SinovadMediaServer.Middleware;
