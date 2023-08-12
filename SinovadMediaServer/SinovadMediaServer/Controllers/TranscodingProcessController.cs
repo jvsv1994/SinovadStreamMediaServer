b0af075ce58fb1a74e47cdf584394fa4f172f7e0
Jorge Victor Solis Vega <jvsv1994@gmail.com>
2023-08-12 17:45:23 -0500

    JS - Enhancements

--- SinovadMediaServer/SinovadMediaServer/Controllers/TranscodingProcessController.cs
+++ SinovadMediaServer/SinovadMediaServer/Controllers/TranscodingProcessController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SinovadMediaServer.Application.DTOs;
 using SinovadMediaServer.Application.Interface.UseCases;
+using System.Text.Json;
 
 namespace SinovadMediaServer.Controllers
 {
@@ -103,15 +104,51 @@ namespace SinovadMediaServer.Controllers
             return BadRequest(response.Message);
         }
 
-        [HttpDelete("DeleteByListGuids/{guids}")]
-        public ActionResult DeleteByListGuids(string guids)
+        [HttpDelete("DeleteByListGuids")]
+        public async Task<ActionResult> DeleteByListGuids([FromQuery] string guids)
         {
-            var response = _transcodingProcessService.DeleteByListGuids(guids);
-            if (response.IsSuccess)
+            try
             {
-                return Ok(response);
+                Dictionary<string, object> data = new Dictionary<string, object>();
+                var list = _transcodingProcessService.DeleteByListGuids(guids);
+                var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(JsonSerializer.Serialize(list));
+                return Ok(Convert.ToBase64String(plainTextBytes));
             }
-            return BadRequest(response.Message);
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpPost("UpdateVideoData")]
+        public async Task<ActionResult> UpdateVideoData([FromBody] TranscodePrepareVideoDto transcodePrepareVideo)
+        {
+            try
+            {
+                TranscodeRunVideoDto transcodeRunVideoDto = _transcodingProcessService.RunProcess(transcodePrepareVideo);
+                var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(JsonSerializer.Serialize(transcodeRunVideoDto));
+                return Ok(Convert.ToBase64String(plainTextBytes));
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpPost("GetVideoData")]
+        public async Task<ActionResult> GetVideoData([FromBody] TranscodePrepareVideoDto transcodeVideoDto)
+        {
+            try
+            {
+                TranscodePrepareVideoDto transcodePrepareVideo = await _transcodingProcessService.PrepareProcess(transcodeVideoDto);
+                TranscodeRunVideoDto transcodeRunVideoDto = _transcodingProcessService.RunProcess(transcodePrepareVideo);
+                var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(JsonSerializer.Serialize(transcodeRunVideoDto));
+                return Ok(Convert.ToBase64String(plainTextBytes));
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
 
     }
